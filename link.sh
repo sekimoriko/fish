25070f9b5c4ec4776d52765a383359d89a8184a9
Koudai-Sekimori <sekimori@iplab.cs.tsukuba.ac.jp>
2021-10-12 16:27:38 +0900

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
 ln -s ./config.fish ~/.config/fish/config.fish
 ln -s ./fish_plugins ~/.config/fish/fish_plugins
 ln -s ./fish_variables ~/.config/fish/fish_variables
-ln -s ./functions ~/.config/fish/functions/
+ln -s ./functions ~/.config/fish/functions
